1c777e41a6c40add2eee5198de936a57fcb78a9a
Mark Surnin <marksurnin@gmail.com>
2015-11-26 13:55:35 +0400

    Just pushing the latest working routes before trying out simple-react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,11 @@ import { Link } from 'react-router'
 import { pushState } from 'redux-router';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    event.preventDefault();
-    console.log("GOt a click");
-    const { dispatch } = this.props;
-
-    dispatch(pushState(null, '/Issues'));
-  }
-
   render() {  
     return (
       <div>
         <h1>This is the App route.</h1>
-        {this.props.children}
-        <a href="#">Go to Issues</a>
+        <Link to="/issues">Go to Issues</Link>
       </div>
     );
   }
